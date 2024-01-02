b7b4ecb84758b1706831408900e7c1a28d752b98
Jose Bastos <jbastos@ualg.pt>
2024-01-02 16:03:54 +0000

    vite.config.js updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/~a12345/EXAME/dist',
+  base: '/~a12345/EXAME/dist/',
   server: {
     port: 8080
   },
